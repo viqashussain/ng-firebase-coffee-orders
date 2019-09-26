ef9dc226c2df1e372c3b28d6006e48b9ab81dafb
viqashussain <viqashussain@hotmail.co.uk>
2019-09-26 15:17:57 +0100

    unsubscribe from coffee orders list upon component being destroyed.

--- src/app/coffee-orders/order-list/order-list.component.ts
+++ src/app/coffee-orders/order-list/order-list.component.ts
@@ -1,30 +1,36 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { OrdersService } from "../../shared/orders.service";
 import { Order } from 'src/app/domain/order';
+import { Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: "app-order-list",
   templateUrl: "./order-list.component.html",
   styleUrls: ["./order-list.component.scss"]
 })
-export class OrderListComponent implements OnInit {
+export class OrderListComponent implements OnInit, OnDestroy {
+  
   constructor(private ordersService: OrdersService) { }
 
-  ngOnInit() {
-    this.getCoffeeOrders();
-  }
-
-  coffeeOrders: any[];
+  coffeeOrders$: Observable<any>;
+  componentDestroyed$: Subject<any> = new Subject();
   displayedColumns: string[] = ['addedByUser', 'orderNumber', 'customerName', 'coffeeOrders', 'complete', 'remove'];
 
-  getCoffeeOrders = () =>
-    this.ordersService
-      .getCoffeeOrders()
-      .subscribe(res => {
-        (this.coffeeOrders = res.map(x => x.payload.doc as any))
-      });
+  ngOnInit() {
+    this.coffeeOrders$ = this.ordersService.getCoffeeOrders().pipe(
+      map(x => x.map(x => x.payload.doc as any)),
+      takeUntil(this.componentDestroyed$)
+    );
+  }
 
   deleteOrder = data => this.ordersService.deleteCoffeeOrder(data.id);
 
   markCompleted = data => this.ordersService.updateCoffeeOrder(data.id);
+
+  ngOnDestroy()
+  {
+    this.componentDestroyed$.next(true);
+    this.componentDestroyed$.complete();
+  }
 }
